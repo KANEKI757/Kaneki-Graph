4be3e8ea039b1acab6c75d0ca3d9f8b8d31c57be
KANEKI757 <reza2008444@gmail.com>
2025-04-15 10:30:54 +0330

    Update login.js

--- admin/assets/js/login.js
+++ admin/assets/js/login.js
@@ -1,1 +1,14 @@
+async function login() {
+  const user = document.getElementById("username").value;
+  const pass = document.getElementById("password").value;
+  const res = await fetch("../data/admins.json");
+  const admins = await res.json();
 
+  const found = admins.find(a => a.username === user && a.password === pass);
+  if (found) {
+    sessionStorage.setItem("admin", user);
+    window.location.href = "dashboard.html";
+  } else {
+    document.getElementById("error-msg").innerText = "نام کاربری یا رمز اشتباه است.";
+  }
+}
